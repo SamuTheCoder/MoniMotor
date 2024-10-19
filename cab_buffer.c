52780417fae3d3bc2300763579aa2e5c1893603d
Henrique Cruz <hmecruz@gmail.com>
2024-10-19 16:58:39 +0100

    changes to cab_buffer.

--- cab_buffer.c
+++ cab_buffer.c
@@ -4,33 +4,36 @@
 //Ex: cab_buffer_t* cab_buffer = (cab_buffer_t*)malloc(sizeof(cab_buffer_t));
 //Only after you must use init
 void cab_buffer_t_init(cab_buffer_t* cab_buffer, uint8_t n_buffers, size_t buffer_size) {
-    cab_buffer->most_recent = 0;
+    cab_buffer->most_recent = -1; // Every buffer is free to use
+    cab_buffer->n_buffers = n_buffers;
 
     cab_buffer->buffer_array = (buffer*)malloc(n_buffers * sizeof(buffer));
     if(cab_buffer->buffer_array == NULL)
     {
-        return NULL;
+        fprintf(stderr, "Failed to allocate memory for buffer array\n");
+        return 1; 
     }
 
     for(int i = 0; i < n_buffers; i++)
     {
-        buffer* buffer = &cab_buffer->buffer_array[i];
-        buffer->ptr = (uint8_t*)malloc(buffer_size);
-        if(buffer->ptr == NULL)
+        buffer* buf = &cab_buffer->buffer_array[i];
+        buf->ptr = (uint8_t*)malloc(buffer_size);
+        if(buf->ptr == NULL)
         {
-            return NULL;
+            fprintf(stderr, "Failed to allocate memory for buffer %d\n", i);            
+            return 1;
         }
-        buffer->size = buffer_size;
-
-        buffer->current_ptr = buffer->ptr;
-        buffer->link_count = 0;
+        buf->size = buffer_size;
+        buf->current_ptr = buf->ptr;
+        buf->link_count = 0;
     }
 }
+
 
 void cab_buffer_t_destroy(cab_buffer_t* cab_buffer){
     if (cab_buffer->buffer_array)
     {
-        for(int i = 0; i < sizeof(cab_buffer->buffer_array)/sizeof(buffer); i++)
+        for(int i = 0; i < sizeof(cab_buffer->buffer_array)/sizeof(buffer); i++) // sizeof(cab_buffer->buffer_array)/sizeof(buffer)
         {
             if(cab_buffer->buffer_array[i].ptr)
             {
@@ -42,35 +45,56 @@ void cab_buffer_t_destroy(cab_buffer_t* cab_buffer){
     free(cab_buffer);
 }
 
-uint8_t cab_buffer_t_write(cab_buffer_t* ptr, uint8_t* stream){
+
+uint8_t cab_buffer_t_write(cab_buffer_t* cab_buffer, uint8_t* stream){
     // For writing, preprocessing task must look for the link_count of the buffer
     // To check if the buffer is being used by other tasks
     // And also never write on the current most recent buffer
 
-    for(int i = 0; i <= sizeof(ptr->buffer_array)/sizeof(buffer); i++){
-        if(ptr->buffer_array[i].link_count == 0 && ptr->most_recent != i){
+    for(int i = 0; i < cab_buffer->n_buffers; i++) // sizeof(cab_buffer->buffer_array)/sizeof(buffer) 
+    {
+        if(cab_buffer->buffer_array[i].link_count == 0 && cab_buffer->most_recent != i) // Nobody is using and not the most recent
+        {
             // Write to buffer
-            memcpy(ptr->buffer_array[i].ptr, stream, ptr->buffer_array[i].size);
-            ptr->most_recent = i;
-            return 1;
+            memcpy(cab_buffer->buffer_array[i].ptr, stream, cab_buffer->buffer_array[i].size);
+            cab_buffer->most_recent = i;
+            return 0;
         }
     }
-    return 0;
+    return 1; // No buffer found
 }
 
-uint8_t* cab_buffer_t_read(cab_buffer_t* ptr){
+
+uint8_t* cab_buffer_t_read(cab_buffer_t* cab_buffer){
     // For reading, tasks must look for the most recently written buffer
     // Doesn't matter if the buffer is in use or not
 
-    for(int i = 0; i < sizeof(ptr->buffer_array)/sizeof(buffer); i++)
+    if (cab_buffer->most_recent >= 0 && cab_buffer->most_recent < cab_buffer->n_buffers) 
     {
-        if(i == ptr->most_recent)
-        {
-            ptr->buffer_array[i].link_count++;
-            return ptr->buffer_array[i].ptr;
-        }
+        cab_buffer->buffer_array[cab_buffer->most_recent].link_count++; // Increase number of users
+        return cab_buffer->buffer_array[cab_buffer->most_recent].ptr;
     }
-    return NULL;
+    return 1; // No buffer found
+}
+
+
+uint8_t cab_buffer_t_print(cab_buffer_t* cab_buff, uint8_t buffer_number) {
+    if (buffer_number >= cab_buff->n_buffers) {
+        fprintf(stderr, "Invalid buffer number\n");
+        return 1;
+    }
+
+    buffer* buf = &cab_buff->buffer_array[buffer_number];
+    printf("Buffer %d:\n", buffer_number);
+    printf("Size: %zu\n", buf->size);
+    printf("Link count: %d\n", buf->link_count);
+    printf("Data: ");
+    for (size_t i = 0; i < buf->size; i++) {
+        printf("%02x ", buf->ptr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
 
--- include/cab_buffer.h
+++ include/cab_buffer.h
@@ -20,10 +20,11 @@
  * @brief Struct that represents a buffer
  */
 typedef struct {
-    uint8_t* ptr;
+    uint8_t* ptr; // pointer to buffer
     size_t size; //size of ptr in bytes
-    uint8_t* current_ptr;
-    uint8_t link_count; //number of tasks using the buffer  
+    uint8_t* current_ptr; // buffer write pointer
+    uint8_t link_count; //number of tasks using the buffer
+    pthread_mutex_t mutex; // Mutex to read and write the link_count safely  
 } buffer;
 
 /**
@@ -31,7 +32,8 @@ typedef struct {
  * @brief Struct that represents a CAB buffer
  */
 typedef struct {
-    buffer* buffer_array;
+    buffer* buffer_array; // ptr to array of buffers
+    uint8_t n_buffers; // number of buffer
     int most_recent; //most recent buffer written
 } cab_buffer_t;
 
@@ -42,6 +44,7 @@ typedef struct {
  * @param cab_buffer Pointer to the CAB buffer
  * @param n_buffers Number of buffers
  * @param buffer_size Size of each buffer
+ * @return 0 if the init was successful
  */
 void cab_buffer_t_init(cab_buffer_t* cab_buffer, uint8_t n_buffers, size_t buffer_size);
 
@@ -62,10 +65,18 @@ uint8_t* cab_buffer_t_read(cab_buffer_t* cab_buff);
  * @brief Writes to a CAB buffer
  * @param cab_buffer Pointer to the CAB buffer
  * @param stream Pointer to the stream to be written
- * @return 1 if the write was successful, 0 otherwise
+ * @return 0 if the write was successful
  */
 uint8_t cab_buffer_t_write(cab_buffer_t* cab_buff, uint8_t* stream);
 
-//falta a quinta
+
+/**
+ * @brief Prints a buffer from CAB buffer
+ * @param cab_buffer Pointer to the CAB buffer
+ * @param buffer_number Number of buffer from the CAB buffer array to print 
+ * @return 0 if the print was successful
+ */
+uint8_t cab_buffer_t_print(cab_buffer_t* cab_buff, uint8_t buffer_number);
+
 
 #endif 
